94110c00c63714cf3cbf91ddc1fa5d00926997d1
aditya2009-coder <saranghaeyo.1302@gmail.com>
2022-06-20 17:20:30 +0530

    Calculator Program

--- WelcomeGLCore/Program.cs
+++ WelcomeGLCore/Program.cs
@@ -7,7 +7,7 @@ int result = operations.Add(10, 10);
 int sub = operations.Subtract(20, 10);
 int multiply = operations.Multiply(10, 10);
 int divide = operations.Divide(100, 10);
-int squareRoot = extraOps.Sq(25);
+int squareRoot = extraOps.Sq(64);
 int AddThree = extraOps.AddNum(5, 10, 15);
 string Company = extraOps.Concat("Global", "Logic");
 Console.WriteLine(result.ToString());
