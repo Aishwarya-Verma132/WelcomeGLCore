5c857ad6521893dc0edee7cfe09169fdc67b44ea
aditya2009-coder <saranghaeyo.1302@gmail.com>
2022-06-20 17:03:27 +0530

    Add project files.

---
+++ CalculatorLibrary/ExtraOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class ExtraOperations
+    {
+        public string Concat(string a, string b)
+        {
+            return a + b;
+        }
+        public int Sq(int a)
+        {
+            return (int)Math.Sqrt(a);
+        }
+        public int AddNum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
---
+++ WelcomeGLCore/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalculatorLibrary;
+
+CalculateOperations operations = new CalculateOperations(); 
+ExtraOperations extraOps = new ExtraOperations();
+int result = operations.Add(10, 10);
+int sub = operations.Subtract(20, 10);
+int multiply = operations.Multiply(10, 10);
+int divide = operations.Divide(100, 10);
+int squareRoot = extraOps.Sq(25);
+int AddThree = extraOps.AddNum(5, 10, 15);
+string Company = extraOps.Concat("Global", "Logic");
+Console.WriteLine(result.ToString());
+Console.WriteLine(sub.ToString());
+Console.WriteLine(multiply.ToString());
+Console.WriteLine(divide.ToString());
+Console.WriteLine(squareRoot.ToString());
+Console.WriteLine(AddThree.ToString());
+Console.WriteLine(Company.ToString());
+Console.ReadLine();
